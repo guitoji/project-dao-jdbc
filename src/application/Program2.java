fe7982b161d22130cf681d050568853dc5fb519f
guitoji <guilherme.limaal22@outlook.com>
2026-03-22 23:54:06 -0300

    DepartmentDaoJDBC insert implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -2,13 +2,17 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
+import model.entities.Department;
 
 public class Program2 {
 
-    static void main() {
+    static void main(String []args) {
 
-        DepartmentDao department = DaoFactory.CreateDepartmentDao();
+        DepartmentDao departmentDao = DaoFactory.CreateDepartmentDao();
 
-
+        IO.println("=== Test 1: deparment insert ===");
+        Department newDepartment = new Department(null, "Games");
+        departmentDao.insert(newDepartment);
+        IO.println("New Id = " + newDepartment.getId());
     }
 }
